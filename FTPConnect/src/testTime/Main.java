74e173f0d90ea3c3c4869ec007fc844be6d61c60
megan.styles <megan.styles@gmail.com>
2017-08-14 15:21:36 +1200

    ftps connected and all ftp methods now transferred to FTPSOperations.class

--- FTPConnect/src/testTime/Main.java
+++ FTPConnect/src/testTime/Main.java
@@ -1,7 +1,8 @@
 package testTime;
 
 import org.apache.commons.net.ftp.FTPFile;
-import org.junit.Test;
+import org.apache.commons.net.ftp.FTPSClient;
+
 
 /**
  * @author stylesm
@@ -9,16 +10,28 @@ import org.junit.Test;
  */
 public class Main {
 
-	@Test
+
 	public static void main(String[] args) {
 
-		FTPSOperations ftpS = new FTPSOperations();
 		
-		System.out.println(ftpS.connectToFTPSServer("195.144.107.198", 21, "demo", "password", 10000));
+		//FTPSOperations ftpS = new FTPSOperations();
+		FTPSClient ftpS = new FTPSClient(false);
+		try {
+			ftpS.setConnectTimeout(10000);
+			ftpS.connect("195.144.107.198", 21);
+			//ftpS.login("demo", "password");
+			
+			
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//System.out.println(ftpS.connectToFTPSServer("195.144.107.198", 21, "demo", "password", 10000));
 	//System.out.println(ftpS.getListOfFiles("/").length);
 		
-		String[] nameArray = ftpS.getListOfFiles("/");
-		System.out.println(nameArray.length);
+		//String[] nameArray = ftpS.getListOfFiles("/");
+	//System.out.println(nameArray.length);
 	/*	for(String name : nameArray) {
 			//String name = file.getName().toString();
 			System.out.println(name);
